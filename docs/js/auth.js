4f3b595b896c179b0854c0d91b0ef6a99a225a0b
Shtopo <65442761+Shtopo@users.noreply.github.com>
2025-06-04 16:37:06 +0300

    Add files via upload

---
+++ docs/js/auth.js
@@ -0,0 +1,23 @@
+import { API_URL } from "./config.js";
+
+document.querySelector("#login-form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+
+  const response = await fetch(`${API_URL}/api/auth/login`, {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await response.json();
+
+  if (response.ok) {
+    alert("Вход выполнен! Ваш токен: " + data.token);
+    localStorage.setItem("authToken", data.token);
+  } else {
+    alert("Ошибка: " + data.message);
+  }
+});
